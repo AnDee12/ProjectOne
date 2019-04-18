55b562a7c4b5dcbed22ca7d815173eaf4eca07c1
andee <ion-al@yandex.ru>
2019-04-18 23:43:35 +0300

    first commit

---
+++ src/main/java/ru/prjone/ProjectOne/main.java
@@ -0,0 +1,11 @@
+package ru.prjone.ProjectOne;
+
+public class mainPrjOne {
+    public static void main(String[] args) {
+        System.out.println("Run main");
+
+
+        System.out.println("Run main23");
+
+    }
+}
